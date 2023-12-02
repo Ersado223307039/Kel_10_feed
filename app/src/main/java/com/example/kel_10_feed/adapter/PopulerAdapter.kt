fd7d463956b4a8fa9ac9b2247056bfefa2a58271
Ersado223307039 <ersadocb2505@gmail.com>
2023-12-02 22:18:23 +0700

    membuat tampilan home

---
+++ app/src/main/java/com/example/kel_10_feed/adapter/PopulerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.kel_10_feed.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kel_10_feed.databinding.PopulerItemBinding
+
+class PopulerAdapter (private val items:List<String>,private val price:List<String>,private val image:List<Int>):RecyclerView.Adapter<PopulerAdapter.PopulerViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopulerViewHolder {
+        return PopulerViewHolder(PopulerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+
+    override fun onBindViewHolder(holder: PopulerViewHolder, position: Int) {
+        val item = items[position]
+        val images = image[position]
+        val price = price[position]
+        holder.bind(item,price,images)
+        
+    }
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class PopulerViewHolder(private val binding: PopulerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        private val imagesView = binding.imageView4
+        fun bind(item: String, price: String, images: Int) {
+            binding.foodNamePopuler.text = item
+            binding.pricePopuler.text =price
+            imagesView.setImageResource(images)
+
+
+        }
+
+
+    }
+
+}
