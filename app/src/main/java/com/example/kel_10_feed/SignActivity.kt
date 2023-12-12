51e79ddc186f1a432f841a90cce754a24fe3c32f
Ersado223307039 <ersadocb2505@gmail.com>
2023-12-12 23:12:13 +0700

    mengubungkan sign in Kedatabase

--- app/src/main/java/com/example/kel_10_feed/SignActivity.kt
+++ app/src/main/java/com/example/kel_10_feed/SignActivity.kt
@@ -3,10 +3,23 @@ package com.example.kel_10_feed
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.kel_10_feed.databinding.ActivityLoginBinding
 import com.example.kel_10_feed.databinding.ActivitySignBinding
+import com.example.kel_10_feed.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SignActivity : AppCompatActivity() {
+    private lateinit var email:String
+    private lateinit var password:String
+    private lateinit var username:String
+    private lateinit var auth:FirebaseAuth
+    private lateinit var database:DatabaseReference
     private val binding: ActivitySignBinding by lazy {
         ActivitySignBinding.inflate(layoutInflater)
     }
@@ -14,10 +27,56 @@ class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth=Firebase.auth
+        database=Firebase.database.reference
+
+        binding.createAccountButton.setOnClickListener {
+            username=binding.userName.text.toString()
+            email=binding.emailAddress.text.toString().trim()
+            password=binding.password.text.toString().trim()
+
+            if(email.isEmpty()||password.isBlank()||username.isBlank()){
+                Toast.makeText(this,"please fill all the detail",Toast.LENGTH_SHORT).show()
+            }else{
+                createAccount(email,password)
+            }
+
+        }
+
         binding.HaveBtn.setOnClickListener {
             val intent = Intent (this,LoginActivity::class.java)
             startActivity(intent)
         }
     }
 
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                Toast.makeText(this,"Account Created Succesfull",Toast.LENGTH_SHORT).show()
+                saveUserData()
+                startActivity(Intent(this,LoginActivity::class.java))
+                finish()
+            }else{
+                Toast.makeText(this,"Account Creation Failed",Toast.LENGTH_SHORT).show()
+                Log.d("Account","createAccount: Failure",task.exception)
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        username=binding.userName.text.toString()
+        password=binding.password.text.toString().trim()
+        email=binding.emailAddress.text.toString().trim()
+
+        val user=UserModel(username,email,password)
+        val userId=FirebaseAuth.getInstance().currentUser!!.uid
+        //save data
+        database.child("user").child(userId).setValue(user)
+
+    }
+
 }
+
+
